301b3c4006340ef13a241a5ab801e608e328d1c4
Yurii Mentorship <fast_yura@ukr.net>
2022-11-13 13:20:51 +0200

    Test commit with message

--- src/main/java/mentorship/App.java
+++ src/main/java/mentorship/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! - git dd" );
+        System.out.println( "Hello World! - git add -2 " );
     }
 }
